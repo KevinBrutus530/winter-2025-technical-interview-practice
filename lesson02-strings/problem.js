00435ba3c0b0bc34d3296fffeab3bb548bb2e405
Kevin Brutus <kevinbrutus@pursuit.org>
2025-01-29 11:38:58 -0500

    lesson 2 and 3 problem complete

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -6,6 +6,20 @@
  */
 function lengthOfLongestSubstring(s) {
   // Your code here
+  const set = new Map();
+
+  let max = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    if (set.get(s[right]) >= left) {
+      left = set.get(s[right]) + 1;
+    } else {
+      max = Math.max(max, right - left + 1);
+    }
+    set.set(s[right], right);
+  }
+  return max;
 }
 
 module.exports = lengthOfLongestSubstring;
--- lesson03-strings/problem.js
+++ lesson03-strings/problem.js
@@ -31,6 +31,30 @@ function stringMatching(text, pattern) {
   // TODO: Implement the string matching algorithm
   // Return an array of indices where the pattern is found in the text
   // If the pattern is not found, return an empty array
+  if (text === "" || pattern === "") return [];
+  let output = [];
+
+
+  // Initial faulty approach:
+  // let regex = new RegExp(pattern, "gi")
+
+  // // for(const match of text.matchAll(regex)){
+  // //   output.push(match.index)
+  // // }
+
+  // Alternative solution:
+  for (let i = 0; i < text.length; i++) {
+    const portion = text.slice(i, i + pattern.length);
+    if (portion === pattern) {
+      output.push(i);
+    }
+  }
+
+  return output;
 }
+
+console.log(stringMatching("ababcababcabc", "abc"));
+console.log(stringMatching("hello world", "abc"));
+console.log(stringMatching("abababab", "aba"));
 
 module.exports = stringMatching;
